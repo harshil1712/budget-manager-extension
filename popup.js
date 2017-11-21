c3fd95b92c52d8c23c68b4a0a28b237f783d9d1d
Harshil Agrawal <ghagrawal17@gmail.com>
2017-11-22 02:59:00 +0530

    Added js

---
+++ popup.js
@@ -0,0 +1,22 @@
+$(function(){
+	$('#spendAmount').click(function(){
+		//Calling the Chrome Storage API to get
+		chrome.storage.sync.get('total',function(budget){
+			var newTotal =0;
+			//checking if total exist
+			if(budget.total){
+				newTotal += parseInt(budget.total);
+			}
+			//Getting datat from the input field
+			var amount = $('#amount').val();
+			if(amount){
+				newTotal += parseInt(amount);
+			}
+
+			//Storing the value 
+			chrome.storage.sync.set({'total':newTotal});
+			$('#total').text(newTotal);
+			$('#amount').val('');
+		});
+	});
+});
